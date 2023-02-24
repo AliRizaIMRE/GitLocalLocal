7b8049a1329320067519c27e6d7a2f89e51ec225
AliRiza <imrealiriza11@gmail.com>
2023-02-24 19:23:32 +0100

    SONN

--- src/Test/RunTest.java
+++ src/Test/RunTest.java
@@ -3,5 +3,19 @@ package Test;
 public class RunTest {
     public static void main(String[] args) {
         System.out.println("TEST");
+
+        // git config -- global.user.name AliRiza
+        // git config -- global.user.email
+        // git init
+        // git add patch
+        // git add .  --> Butun dosyalari ekler --> kanca atar
+        // clear yada CTRL + L terminali temizler
+        // git commit -m"mesajini ekle" --> degisikler ekleme cikarmalar
+        // git status --> Degisiklikler
+        // Proje de dosya olustur ---> .gitignore --> Buraya ignore edeceginiz dosyalari ekle
+        // Proje de dosya olustur ---> README.md --> GitHub ta projeleri okutmak icin
+
+        // README.md ---> Projeye fihrist github icin
+
     }
 }
