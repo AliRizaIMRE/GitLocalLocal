aae01cfc99cd7899f77c3fe0396b3d537f713c39
AliRiza <imrealiriza11@gmail.com>
2023-02-27 18:28:01 +0100

    RunTest Uploaded

--- src/Test/RunTest.java
+++ src/Test/RunTest.java
@@ -6,11 +6,10 @@ public class RunTest {
 
         // git config -- global.user.name AliRiza
         // git config -- global.user.email
-        // git init
-        // git add patch
-        // git add .  --> Butun dosyalari ekler --> kanca atar
-        // clear yada CTRL + L terminali temizler
-        // git commit -m"mesajini ekle" --> degisikler ekleme cikarmalar
+        // git init --> dosyayi initialize etti kancaladi  (initialize)
+        // git add patch ---> Staging area'ya atti         (staging area)
+        // git add .  --> Butun dosyalari ekler -->
+        // git commit -m"mesajini ekle" --> degisikler ekleme cikarmalar  (Local Host'a atar)
         // git log --> Log kayitlarina bakabiliriz --> :wq --> logtan cikar
         // git status --> Degisiklikler
         // git remote add origin https://github.com/AliRizaIMRE/GitLocalLocal.git ---> gitHup a entegrasyon icin
@@ -18,8 +17,8 @@ public class RunTest {
         // git branch --> branch'lari görme
         // git push -u origin main --- local deki branch imi zorla maine push et
         // git branch ---> mevcut branslari gör
-        // git branch test ---> test diye bir branch olustu
-
+        // git branch test ---> test diye bir branch olustur
+        // clear yada CTRL + L terminali temizler
 
         // Proje de dosya olustur ---> .gitignore --> Buraya ignore edeceginiz dosyalari ekle
         // Proje de dosya olustur ---> README.md --> GitHub ta projeleri okutmak icin
